d9ece624303067d924198b61181077c2d575dfd7
RosalynS78 <rrcervania78@gmail.com>
2022-08-16 21:19:32 -0500

    Updates

---
+++ precache-manifest.df0f2c896b3fa97b338a03b97fd01a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43dda85e62da489ba633",
+    "url": "/static/css/main.23c701e6.chunk.css"
+  },
+  {
+    "revision": "43dda85e62da489ba633",
+    "url": "/static/js/main.43dda85e.chunk.js"
+  },
+  {
+    "revision": "8a8cc8e8ff45c8a412ed",
+    "url": "/static/js/1.8a8cc8e8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3ea9b3059fbe6a3864043f0bdfa1f1d",
+    "url": "/index.html"
+  }
+];
